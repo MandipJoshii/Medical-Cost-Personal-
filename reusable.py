adad4892d134cc28b41f5adbe9c26a00310c3789
mandipjoshii <mandip.joshi824@gmail.com>
2026-03-12 22:26:53 +0545

    basic eda

--- reusable.py
+++ reusable.py
@@ -24,3 +24,35 @@ def oneHot_encoder(df):
 def cleaning(df):
     df = df.drop_duplicates()
     return df
+
+
+
+
+def binning(df):
+    def bmi_category(bmi):
+        if bmi < 18.5:
+         return 'underweight'
+        elif bmi < 25:   
+         return 'normal'   
+        elif bmi < 30:
+         return 'overweight'
+        elif bmi >= 30:
+         return 'obese' 
+
+    df['bmi_category'] = df['bmi'].apply(bmi_category) 
+    return df
+
+
+
+def bmi_category(df):
+       if 'bmi_category' in df.columns:
+        encoding = pd.get_dummies(df['bmi_category'], drop_first=True,dtype=int)
+        df = pd.concat([df,encoding],axis=1)
+        df = df.drop(columns=['bmi_category'])
+        return df
+
+
+
+def flag(df):
+   df['high_risks'] = ((df['smoker'] == 1) & (df['bmi'] > 30)).astype(int)
+   return df
